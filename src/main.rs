63a3d94aaba5148dc9606c54a9d7f068d225c0e2
Francesco Cerroni <francescocerroni03@gmail.com>
2022-08-11 14:23:24 +0200

    added display and sum traits implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+// use std::ops;
+use std::fmt;
+use std::ops;
+
+struct Measure<T> {
+    value: T,
+    error: T
+}
+
+impl<T: fmt::Display> fmt::Display for Measure<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} ± {}", self.value, self.error)
+    }
+}
+
+impl<T: ops::Add<Output = T>> ops::Add for Measure<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            value: self.value + other.value,
+            error: self.error + other.error
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m1 = Measure::<f32> {
+        value: 10.58,
+        error: 0.018
+    };
+    let m2 = Measure::<f32> {
+        value: 6.00,
+        error: 0.300
+    };
+
+    println!("{}", m1 + m2);
 }
