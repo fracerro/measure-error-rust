92606c23a42e676394890a30cf488160ee779a04
Francesco Cerroni <francescocerroni03@gmail.com>
2022-08-11 14:39:09 +0200

    added multiplication and division

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ struct Measure<T> {
     error: T
 }
 
+impl<T: Copy + ops::Div> Measure<T> {
+    fn relative_error(&self) -> <T as ops::Div>::Output {
+        return self.error / self.value;
+    }
+}
+
 impl<T: fmt::Display> fmt::Display for Measure<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} ± {}", self.value, self.error)
@@ -24,6 +30,28 @@ impl<T: ops::Add<Output = T>> ops::Add for Measure<T> {
     }
 }
 
+impl<T: ops::Add<Output = T> + ops::Sub<Output = T>> ops::Sub for Measure<T> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            value: self.value - other.value,
+            error: self.error + other.error
+        }
+    }
+}
+
+impl<T: Copy + ops::Add<Output = T> + ops::Mul<Output = T> + ops::Div<Output = T>> ops::Mul for Measure<T> {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            value: self.value * other.value,
+            error: (self.relative_error() + other.relative_error()) * (self.value * other.value)
+        }
+    }
+}
+
 fn main() {
     let m1 = Measure::<f32> {
         value: 10.58,
@@ -34,5 +62,5 @@ fn main() {
         error: 0.300
     };
 
-    println!("{}", m1 + m2);
+    println!("{}", m1 * m2);
 }
